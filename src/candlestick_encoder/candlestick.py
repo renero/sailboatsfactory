abe31e22c6db44f1a2c545070293a001c368f3d0
Jesus Renero <jesus.renero@gmail.com>
2018-06-10 18:40:07 +0200

    Added encoding method.

--- src/candlestick_encoder/candlestick.py
+++ src/candlestick_encoder/candlestick.py
@@ -62,8 +62,17 @@ class Candlestick:
         if self.shadows_symmetric is True and self.body_relative_size > self.min_relative_size:
             self.body_in_center = True
 
-    def __init__(self, ohlc):
+    def correct_encoding(self):
+        """
+        Check if the encoding proposed has all elements (OHLC)
+        :return: True or False
+        """
+        # check that we have all letters
+        return all(self.encoding.find(c) != -1 for c in 'OHLC')
+
+    def __init__(self, values, encoding="ohlc"):
         # Set all object variables to 0 or false.
+        self.encoding = encoding.upper()
         self.open = self.close = self.high = self.low = 0.0
         self.min = self.max = self.min_percentile = self.max_percentile = 0.0
         self.mid_body_percentile = self.mid_body_point = 0.0
@@ -77,11 +86,56 @@ class Candlestick:
         self.shadows_relative_diff = 0.0
 
         # Assign the proper values to them
-        self.open = ohlc[0]
-        self.high = ohlc[1]
-        self.low = ohlc[2]
-        self.close = ohlc[3]
+        if self.correct_encoding() is False:
+            raise ValueError('Could not find all mandatory chars (o, h, l, c) in encoding ({})'.format(self.encoding))
+        self.open = values[self.encoding.find('O')]
+        self.high = values[self.encoding.find('H')]
+        self.low = values[self.encoding.find('L')]
+        self.close = values[self.encoding.find('C')]
         self.calc_parameters()
+
+    def encode_with(self, encoding_substring):
+        if self.body_in_center:
+            print('  centered')
+            return encoding_substring[0]
+        else:
+            if self.has_both_shadows:
+                if self.body_in_upper_half:
+                    return encoding_substring[1]
+                else:
+                    if self.body_in_lower_half:
+                        return encoding_substring[2]
+                    else:
+                        raise ValueError('Body centered & 2 shadows but not in upper or lower halves')
+            else:
+                if self.has_lower_shadow:
+                    return encoding_substring[3]
+                else:
+                    return encoding_substring[4]
+
+    def encode_body(self):
+        if self.body_relative_size <= self.min_relative_size:
+            return self.encode_with('ABCDE')
+        else:
+            if self.body_relative_size <= 0.1 + 0.05:
+                print('  10%')
+                return self.encode_with('FGHIJ')
+            else:
+                if self.body_relative_size <= 0.25 + 0.1:
+                    print('  25%')
+                    return self.encode_with('KLMNO')
+                else:
+                    if self.body_relative_size <= 0.5 + 0.1:
+                        print('  50%')
+                        return self.encode_with('PPPQR')
+                    else:
+                        if self.body_relative_size <= 0.75 + 0.2:
+                            print('  75%')
+                            return self.encode_with('SSSTU')
+                        else:
+                            print('  ~ 100%')
+                            return 'V'
+
 
     def info(self):
         print('body center percentile: {:0.2f}'.format(self.mid_body_percentile))
@@ -89,6 +143,6 @@ class Candlestick:
         print('body in upper half? {}'.format(self.body_in_upper_half))
         print('body in lower half? {}'.format(self.body_in_lower_half))
         print('body relative width: {:0.2f}'.format(self.body_relative_size))
-        print('up/lw shdws wdth: {:0.2f}/{:0.2f}'.format(self.upper_shadow_percentile, self.lower_shadow_percentile))
+        print('up/lw shdws width: {:0.2f}/{:0.2f}'.format(self.upper_shadow_percentile, self.lower_shadow_percentile))
         print('upper/lower shadows? {}/{}'.format(self.has_upper_shadow, self.has_lower_shadow))
         print('shadows are symmetric: {}'.format(self.shadows_symmetric))
--- src/candlestick_encoder/main.py
+++ src/candlestick_encoder/main.py
@@ -4,8 +4,9 @@ from candlestick import Candlestick
 
 
 def main():
-    c = Candlestick(np.array([1.0, 10.0, 0.0, 9.0]))
+    c = Candlestick(np.array([1.0, 10.0, 0.0, 9.0]), "ohlc")
     c.info()
+    print('Encoding body as: {}'.format(c.encode_body()))
 
 
 if __name__== "__main__":
