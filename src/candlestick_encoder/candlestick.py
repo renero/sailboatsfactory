57ee730f971c15cde164d1e3b023d2b199da2e48
Jesus Renero <jesus.renero@gmail.com>
2018-08-26 12:45:06 +0200

    Candlestick predictor refactor to take params

--- src/candlestick_encoder/candlestick.py
+++ src/candlestick_encoder/candlestick.py
@@ -3,6 +3,32 @@ class Candlestick:
     min_relative_size = 0.02
     shadow_symmetry_diff_threshold = 0.1
 
+    def __init__(self, values, encoding="ohlc"):
+        # Set all object variables to 0 or false.
+        self.encoding = encoding.upper()
+        self.open = self.close = self.high = self.low = 0.0
+        self.min = self.max = self.min_percentile = self.max_percentile = 0.0
+        self.mid_body_percentile = self.mid_body_point = 0.0
+        self.positive = self.negative = False
+        self.has_upper_shadow = self.has_lower_shadow = self.has_both_shadows = False
+        self.shadows_symmetric = False
+        self.body_in_upper_half = self.body_in_lower_half = self.body_in_center = False
+        self.hl_interval_width = self.upper_shadow_len = self.lower_shadow_len = 0.0
+        self.upper_shadow_percentile = self.lower_shadow_percentile = 0.0
+        self.oc_interval_width = self.body_relative_size = 0.0
+        self.shadows_relative_diff = 0.0
+
+        # Assign the proper values to them
+        if self.correct_encoding() is False:
+            raise ValueError(
+                'Could not find all mandatory chars (o, h, l, c) in encoding ({})'.
+                format(self.encoding))
+        self.open = values[self.encoding.find('O')]
+        self.high = values[self.encoding.find('H')]
+        self.low = values[self.encoding.find('L')]
+        self.close = values[self.encoding.find('C')]
+        self.calc_parameters()
+
     @staticmethod
     def div(x, y):
         if y == 0.0:
@@ -27,32 +53,41 @@ class Candlestick:
         # Mid point of the body (absolute value)
         self.mid_body_point = self.min + (self.oc_interval_width / 2.0)
         # Percentile of the body (relative)
-        self.mid_body_percentile = self.div((self.mid_body_point - self.low), self.hl_interval_width)
+        self.mid_body_percentile = self.div((self.mid_body_point - self.low),
+                                            self.hl_interval_width)
 
         # Calc the percentile position of min and max values
-        self.min_percentile = self.div((self.min - self.low), self.hl_interval_width)
-        self.max_percentile = self.div((self.max - self.low), self.hl_interval_width)
+        self.min_percentile = self.div((self.min - self.low),
+                                       self.hl_interval_width)
+        self.max_percentile = self.div((self.max - self.low),
+                                       self.hl_interval_width)
 
         # total candle interval range width and shadows lengths
         self.upper_shadow_len = self.high - self.max
-        self.upper_shadow_percentile = self.div(self.upper_shadow_len, self.hl_interval_width)
+        self.upper_shadow_percentile = self.div(self.upper_shadow_len,
+                                                self.hl_interval_width)
         self.lower_shadow_len = self.min - self.low
-        self.lower_shadow_percentile = self.div(self.lower_shadow_len, self.hl_interval_width)
+        self.lower_shadow_percentile = self.div(self.lower_shadow_len,
+                                                self.hl_interval_width)
 
         # Percentage of HL range occupied by the body.
-        self.body_relative_size = self.div(self.oc_interval_width, self.hl_interval_width)
+        self.body_relative_size = self.div(self.oc_interval_width,
+                                           self.hl_interval_width)
 
         # Upper and lower shadows are larger than 2% of the interval range len?
-        if self.div(self.upper_shadow_len, self.hl_interval_width) > self.min_relative_size:
+        if self.div(self.upper_shadow_len,
+                    self.hl_interval_width) > self.min_relative_size:
             self.has_upper_shadow = True
-        if self.div(self.lower_shadow_len, self.hl_interval_width) > self.min_relative_size:
+        if self.div(self.lower_shadow_len,
+                    self.hl_interval_width) > self.min_relative_size:
             self.has_lower_shadow = True
         if self.has_upper_shadow and self.has_lower_shadow:
             self.has_both_shadows = True
 
-        # Determine if body is centered in the interval. It must has two shadows with a difference
-        # in their lengths less than 5% (param).
-        self.shadows_relative_diff = abs(self.upper_shadow_percentile - self.lower_shadow_percentile)
+        # Determine if body is centered in the interval. It must has
+        # two shadows with a difference in their lengths less than 5% (param).
+        self.shadows_relative_diff = abs(
+            self.upper_shadow_percentile - self.lower_shadow_percentile)
         if self.has_both_shadows is True:
             if self.shadows_relative_diff < self.shadow_symmetry_diff_threshold:
                 self.shadows_symmetric = True
@@ -65,7 +100,10 @@ class Candlestick:
         if self.shadows_symmetric is True and self.body_relative_size > self.min_relative_size:
             self.body_in_center = True
         # None of the above is fulfilled...
-        if any([self.body_in_center, self.body_in_lower_half, self.body_in_upper_half]) is False:
+        if any([
+                self.body_in_center, self.body_in_lower_half,
+                self.body_in_upper_half
+        ]) is False:
             if self.lower_shadow_percentile > self.upper_shadow_percentile:
                 self.body_in_upper_half = True
             else:
@@ -79,33 +117,9 @@ class Candlestick:
         # check that we have all letters
         return all(self.encoding.find(c) != -1 for c in 'OHLC')
 
-    def __init__(self, values, encoding="ohlc"):
-        # Set all object variables to 0 or false.
-        self.encoding = encoding.upper()
-        self.open = self.close = self.high = self.low = 0.0
-        self.min = self.max = self.min_percentile = self.max_percentile = 0.0
-        self.mid_body_percentile = self.mid_body_point = 0.0
-        self.positive = self.negative = False
-        self.has_upper_shadow = self.has_lower_shadow = self.has_both_shadows = False
-        self.shadows_symmetric = False
-        self.body_in_upper_half = self.body_in_lower_half = self.body_in_center = False
-        self.hl_interval_width = self.upper_shadow_len = self.lower_shadow_len = 0.0
-        self.upper_shadow_percentile = self.lower_shadow_percentile = 0.0
-        self.oc_interval_width = self.body_relative_size = 0.0
-        self.shadows_relative_diff = 0.0
-
-        # Assign the proper values to them
-        if self.correct_encoding() is False:
-            raise ValueError('Could not find all mandatory chars (o, h, l, c) in encoding ({})'.format(self.encoding))
-        self.open = values[self.encoding.find('O')]
-        self.high = values[self.encoding.find('H')]
-        self.low = values[self.encoding.find('L')]
-        self.close = values[self.encoding.find('C')]
-        self.calc_parameters()
-
     def encode_with(self, encoding_substring):
         if self.body_in_center:
-            #print('  centered')
+            # print('  centered')
             return encoding_substring[0]
         else:
             if self.has_both_shadows:
@@ -115,7 +129,9 @@ class Candlestick:
                     if self.body_in_lower_half:
                         return encoding_substring[2]
                     else:
-                        raise ValueError('Body centered & 2 shadows but not in upper or lower halves')
+                        raise ValueError(
+                            'Body centered & 2 shadows but not in upper or lower halves'
+                        )
             else:
                 if self.has_lower_shadow:
                     return encoding_substring[3]
@@ -127,22 +143,22 @@ class Candlestick:
             return self.encode_with('ABCDE')
         else:
             if self.body_relative_size <= 0.1 + 0.05:
-                #print('  10%')
+                # print('  10%')
                 return self.encode_with('FGHIJ')
             else:
                 if self.body_relative_size <= 0.25 + 0.1:
-                    #print('  25%')
+                    # print('  25%')
                     return self.encode_with('KLMNO')
                 else:
                     if self.body_relative_size <= 0.5 + 0.1:
-                        #print('  50%')
+                        # print('  50%')
                         return self.encode_with('PQRST')
                     else:
                         if self.body_relative_size <= 0.75 + 0.1:
-                            #print('  75%')
+                            # print('  75%')
                             return self.encode_with('UVWXY')
                         else:
-                            #print('  ~ 100%')
+                            # print('  ~ 100%')
                             return 'Z'
 
     def encode_body(self):
@@ -153,19 +169,26 @@ class Candlestick:
         return first_letter + self.__encode_body()
 
     def info(self):
-        print('O({:.3f}), H({:.3f}), L({:.3f}), C({:.3f})'.format(self.open, self.high, self.low, self.close))
+        print('O({:.3f}), H({:.3f}), L({:.3f}), C({:.3f})'.format(
+            self.open, self.high, self.low, self.close))
         print('mid body point: {:0.2f}'.format(self.mid_body_point))
-        print('body center percentile: {:0.2f}'.format(self.mid_body_percentile))
-        print('body min/max percentiles: {:0.2f}/{:0.2f}'.format(self.min_percentile, self.max_percentile))
+        print('body center percentile: {:0.2f}'.format(
+            self.mid_body_percentile))
+        print('body min/max percentiles: {:0.2f}/{:0.2f}'.format(
+            self.min_percentile, self.max_percentile))
         print('body centered? {}'.format(self.body_in_center))
         print('body in upper half? {}'.format(self.body_in_upper_half))
         print('body in lower half? {}'.format(self.body_in_lower_half))
         print('body relative width: {:0.2f}'.format(self.body_relative_size))
-        print('up/lw shdws width: {:0.2f}/{:0.2f}'.format(self.upper_shadow_percentile, self.lower_shadow_percentile))
-        print('up/lw relative diff: {:0.2f}'.format(self.shadows_relative_diff))
-        print('upper/lower shadows? {}/{}'.format(self.has_upper_shadow, self.has_lower_shadow))
+        print('up/lw shdws width: {:0.2f}/{:0.2f}'.format(
+            self.upper_shadow_percentile, self.lower_shadow_percentile))
+        print('up/lw relative diff: {:0.2f}'.format(
+            self.shadows_relative_diff))
+        print('upper/lower shadows? {}/{}'.format(self.has_upper_shadow,
+                                                  self.has_lower_shadow))
         print('shadows are symmetric: {}'.format(self.shadows_symmetric))
         print('')
 
     def values(self):
-        print('O({:.3f}), H({:.3f}), L({:.3f}), C({:.3f})'.format(self.open, self.high, self.low, self.close))
+        print('O({:.3f}), H({:.3f}), L({:.3f}), C({:.3f})'.format(
+            self.open, self.high, self.low, self.close))
---
+++ src/candlestick_encoder/cse.py
@@ -0,0 +1,66 @@
+"""Candle Stick Encoder.
+
+Usage:
+  cse.py [-i <input_filename] [-o <output_name>] [-e <encoding>]
+         [-p <parameters_file>]
+  cse.py (-h | --help)
+  cse.py --version
+
+Options:
+  -i <input>        Input CSV file to be used
+  -o <output>       Input CSV file to be used
+  -e <encoding>     Encoding of input file (closing, high, low, open)
+  -p <params_file>  Path to the parameters file to be used
+  -v --version      Show version.
+  -h --help         Show this screen.
+
+"""
+import sys
+
+import numpy as np
+import pandas
+import yaml
+from docopt import docopt
+
+from candlestick import Candlestick
+
+
+def read(filename):
+    df = pandas.read_csv(filename, sep='|', header=0, usecols=[0, 1, 2, 3])
+    max_value = df.values.max()
+    min_value = df.values.min()
+
+    def normalize(x):
+        return (x - min_value) / (max_value - min_value)
+
+    df = df.applymap(np.vectorize(normalize))
+    return df.round(4)
+
+
+def main(arguments):
+    prms_fname = 'params.yaml' if arguments['-p'] is None else arguments['-p']
+    with open(prms_fname, 'r') as params_file:
+        try:
+            params = yaml.load(params_file)
+            # Overwrite parameters from file if specified through cmd line.
+            if arguments['-i'] is not None:
+                params['input_filename'] = arguments['-i']
+            if arguments['-o'] is not None:
+                params['output_filename'] = arguments['-o']
+            if arguments['-e'] is not None:
+                params['encoding'] = arguments['-e']
+        except yaml.YAMLError as exc:
+            print(exc)
+            sys.exit(1)
+
+    df = read(params['input_filename'])
+    for i in range(df.shape[0]):
+        c = Candlestick(df.iloc[i], params['encoding'])
+        print("{}, {}".format(i, c.encode_body()))
+    print()
+
+
+# sys.argv = ['cse.py', '-i', '/path/tpo/file.ext']
+if __name__ == "__main__":
+    arguments = docopt(__doc__, version='Candlestick Encoder 0.1')
+    main(arguments)
