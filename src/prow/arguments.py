eb3001a521c4e333d9a44db3110745a5372d5420
Jesus Renero <jesus.renero@gmail.com>
2018-11-07 08:02:22 +0100

    added mutually exclusive arguments (partial)

--- src/prow/arguments.py
+++ src/prow/arguments.py
@@ -2,17 +2,28 @@ import argparse
 
 
 class Arguments(object):
-
     args = None
 
     def __init__(self):
+        ActionHelp = """
+            Start = Starts the daemon (default)
+            Stop = Stops the daemon
+            Restart = Restarts the daemon
+            """
         parser = argparse.ArgumentParser()
-        parser.add_argument('-t', nargs=1, type=str,
+
+        parser.add_argument('action', nargs='1', default='predict',
+                            choices=('train', 'predict', 'ensemble'),
+                            help=ActionHelp)
+        parser.add_argument('-t', '--ticks', nargs=1, type=str,
                             help='Ticks file to process')
-        parser.add_argument('-w', nargs=1, type=int,
+        parser.add_argument('-w', '--window', nargs=1, type=int,
                             help='Window size for the LSTM')
 
         self.args = parser.parse_args()
+        action_name = '_{}'.format(self.args.action)
+        setattr(self, action_name, True)
+
 
     @property
     def arg_ticks_file(self):
